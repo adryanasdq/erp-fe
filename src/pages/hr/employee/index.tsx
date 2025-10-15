52369207465d9b6caeb443eb448f91c7a6d789bb
Adryan Ashidiq <adryanashidiq.analyst@gmail.com>
2025-10-15 19:22:20 +0700

    (REFACTOR) Move filter, button modal, page size to Employee Table component

--- src/pages/hr/employee/index.tsx
+++ src/pages/hr/employee/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo, useState } from "react";
-import { Plus } from "lucide-react";
 
 import useStore from "@/models/stores/index";
 import { IEmployee } from "@/models/types/hr/employee";
@@ -17,7 +16,6 @@ const HRPage = () => {
     const [selectedEmployee, setSelectedEmployee] = useState<IEmployee | null>(null);
     const [formDialogMode, setFormDialogMode] = useState<"new" | "edit" | "view">("new");
     const [searchText, setSearchText] = useState("");
-    const [pageSize, setPageSize] = useState(10);
 
     const filteredEmployee = useMemo(() => {
         return data.filter((emp) => {
@@ -31,10 +29,6 @@ const HRPage = () => {
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchText(e.target.value);
-    }
-
-    const handlePageSize = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setPageSize(Number(e.target.value));
     }
 
     const handleEdit = (empId: string) => {
@@ -67,49 +61,14 @@ const HRPage = () => {
 
             <div className="text-4xl mb-4">Employee</div>
             <EmployeeSummary />
-            <div className="flex justify-between mb-4">
-                <button className="btn mr-2" onClick={openModal}>
-                    <Plus /> New
-                </button>
-                <label className="input mb-2">
-                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g
-                            strokeLinejoin="round"
-                            strokeLinecap="round"
-                            strokeWidth="2.5"
-                            fill="none"
-                            stroke="currentColor"
-                        >
-                            <circle cx="11" cy="11" r="8"></circle>
-                            <path d="m21 21-4.3-4.3"></path>
-                        </g>
-                    </svg>
-                    <input
-                        type="search"
-                        className="grow"
-                        placeholder="Search"
-                        value={searchText}
-                        onChange={handleSearch}
-                    />
-                </label>
-
-                <div className="flex items-center mx-4">
-                    <div>Show</div>
-                    <select value={pageSize} className="select mx-2" onChange={handlePageSize}>
-                        <option>5</option>
-                        <option>10</option>
-                        <option>25</option>
-                        <option>100</option>
-                    </select>
-                    <div>entries</div>
-                </div>
-            </div>
 
             <EmployeeTable
                 data={filteredEmployee}
-                pageSize={pageSize}
+                openModal={openModal}
                 onEdit={handleEdit}
                 onDelete={handleDelete}
+                searchText={searchText}
+                handleSearch={handleSearch}
             />
         </>
     );
